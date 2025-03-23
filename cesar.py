296da883cafe270b9e031f5f40a8bb31627f925d
YassinEzaouibi <zouibiyassin03@gmail.com>
2025-03-23 14:53:18 +0000

    cesar cipher basic technic added

---
+++ cesar.py
@@ -0,0 +1,20 @@
+user_input_text = str(input("Saisir le mot pour chiffre:"))
+user_input_key = input("Saisir le Key:")[0]
+C=list()
+for i in user_input_text:
+    if i.isupper():
+            c=chr((((ord(i)-ord('A'))+(ord(user_input_key)-ord('A')))%26)+ord('A'))
+            C.append(c)
+
+    elif i.islower():
+
+        c=chr((((ord(i)-ord('a'))+(ord(user_input_key)-ord('A')))%26)+ord('a'))
+        C.append(c)
+
+    else:
+        C.append("")
+
+print(C)
+message_chiff="".join(C)
+print(message_chiff)
+
